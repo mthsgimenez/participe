51cd4cffa4dea08ac40fa0cba153cb633782d5b3
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2025-11-13 10:02:16 -0300

    feat: package para gerar e validar tokens jwt

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/mthsgimenez/participe
 
 go 1.25.3
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.44.0
+)
---
+++ internal/auth/jwt.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/mthsgimenez/participe/internal/env"
+)
+
+var key = env.GetStringFallback("SECRET_KEY", "secret")
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(email string) (string, error) {
+	expirationDate := time.Now().Add(7 * (time.Hour * 24))
+
+	claims := &Claims{
+		Email: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationDate),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Issuer:    "api",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(key)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func VerifyJWT(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
+		return key, nil
+	})
+
+	if err != nil {
+		if err == jwt.ErrTokenExpired {
+			return nil, fmt.Errorf("token expired")
+		}
+		return nil, fmt.Errorf("could not parse token: %v", err)
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return claims, nil
+}
